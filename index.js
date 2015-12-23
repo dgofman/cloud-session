df97456405dd4c1b20382eb06ab21e3ba68b3033
dgofman <dgofman@gmail.com>
2015-12-23 01:16:02 -0800

    version 0.0.1

---
+++ index.js
@@ -0,0 +1,176 @@
+'use strict';
+
+var crypto = require('crypto'),
+	fs = require('fs'),
+	debug = require('debug')('cloud-session:index'),
+	dns = require('dns'),
+	sessionStore = {},
+	ipaddress;
+
+var ACTION = {
+	SESSION: 'get_session',
+	TRANSFER: 'transfer_sesssion',
+	DESTROY: 'destroy_session'
+};
+
+dns.lookup(require('os').hostname(), function (err, ip) {
+	/* istanbul ignore next */
+	if (!err) {
+		ipaddress = ip;
+	} else {
+		console.error(err);
+	}
+});
+
+module.exports = function(app, portNumber, opt, proxy) {
+	opt = opt || {};
+	proxy = proxy || require('proxy-orchestrator');
+	var apis = {},
+		isHTTPS = !!opt.isHTTPS,
+		isEnvSession = !!opt['session-env'] && ((process.env.NODE_ENV || '').trim() === 'development'),
+		sessionKey = opt['session-key'] || 'cl0udS3sS10nK3y',
+		sessionName =  opt['session-name'] || 'x-cloud-session',
+		expTime = opt['exp-time'] || 3600, //session expiration time in seconds
+		expInterval = opt['exp-interval'] || 60 * 10, //check exired sessions every 10 minutes
+		peer2peer = opt['peer2peer'] || '/cloud-session',
+		lastSessionFile = opt['session-file'] || './session',
+		lastSessionId = null,
+		sidRegExp = new RegExp(sessionName + '=(\\w+)'),
+		ipRegExp = new RegExp('x-cloud-ipaddress=([0-9|\.]+)');
+
+	apis.serialize = function(req, res, data) {
+		return JSON.stringify(data || '');
+	};
+
+	apis.deserialize = function(req, res, data) {
+		return JSON.parse(data || '{}');
+	};
+
+	apis.encrypt = function(val, secret) {
+		return crypto.createHmac('sha256', secret).update(val).digest('base64');
+	};
+
+	apis.destroy = function(req, next) {
+		var match = ipRegExp.exec(req.headers.cookie);
+		if (match && match.length > 1) {
+			proxy({
+				host: match[1],
+				port: portNumber,
+				secure: isHTTPS
+			}, req).request(function(err) {
+				next(err);
+			}, 'POST', peer2peer, {}, {sessionKey: sessionKey, action: ACTION.DESTROY});
+		}
+	};
+
+	apis.cleanAll = function() {
+		var now = Date.now();
+		for (var sessionId in sessionStore) {
+			if (now - sessionStore[sessionId].time > (expTime * 1000)) {
+				debug('Destroy sessionId: ' + sessionId);
+				/* istanbul ignore next */ 
+				if (sessionId === lastSessionId) {
+					fs.unlink(lastSessionFile);
+				}
+				delete sessionStore[sessionId];
+			}
+		}
+	};
+
+	app.post(peer2peer, function (req, res) {
+		var data = null;
+		debug('Host:' + req.headers.host + ', action: ' + req.query.action + ', isvalid: ' + (req.query.sessionKey === sessionKey));
+		if (req.headers && req.headers.cookie && req.query.sessionKey === sessionKey) {
+			var match = sidRegExp.exec(req.headers.cookie);
+			if (match && match.length > 1) {
+				var sessionID = apis.encrypt(match[1], req.query.sessionKey);
+
+				if (req.query.action === ACTION.SESSION || req.query.action === ACTION.TRANSFER) {
+					try {
+						if (sessionStore[sessionID]) {
+							data = apis.serialize(req, res, sessionStore[sessionID].data);
+						}
+					} catch(e) {
+						/* istanbul ignore next */ 
+						console.error(e.stack);
+					}
+				}
+
+				if (req.query.action === ACTION.DESTROY || req.query.action === ACTION.TRANSFER) {
+					delete sessionStore[sessionID];
+				}
+			}
+		}
+		res.send(data);
+	});
+
+	app.use(function session(req, res, next) {
+		if (req.session) {
+			return next();
+		}
+
+		if (!req.sessionID) {
+			if (req.headers && req.headers.cookie) {
+				var match = sidRegExp.exec(req.headers.cookie);
+				if (match && match.length > 1) {
+					try {
+						req.sessionID = apis.encrypt(match[1], sessionKey);
+						if (isEnvSession && !sessionStore[req.sessionID] && fs.existsSync(lastSessionFile)) {
+							sessionStore[req.sessionID] = apis.deserialize(req, res, fs.readFileSync(lastSessionFile, 'utf8'));
+						}
+					} catch(e) {
+						/* istanbul ignore next */
+						console.error(e.stack);
+					}
+				}
+			}
+		}
+
+		if (!req.sessionID) {
+			var uid = crypto.randomBytes(32).toString('hex').slice(0, 32);
+			res.cookie(sessionName, uid);
+			req.sessionID = apis.encrypt(uid, sessionKey);
+		}
+
+		sessionStore[req.sessionID] = sessionStore[req.sessionID] || {
+			time: null,
+			data: {}
+		};
+
+		sessionStore[req.sessionID].time = Date.now();
+
+		if (isEnvSession) {
+			lastSessionId = req.sessionID;
+			fs.writeFileSync(lastSessionFile, apis.serialize(req, res, sessionStore[req.sessionID]));
+		}
+
+		if (req.headers.cookie && req.headers.cookie.indexOf('x-cloud-ipaddress') === -1) {
+			res.cookie('x-cloud-ipaddress', ipaddress);
+		}
+
+		if ((match = ipRegExp.exec(req.headers.cookie)) && match.length > 1 && match[1] !== ipaddress) {
+			res.cookie('x-cloud-ipaddress', ipaddress);
+
+			proxy({
+				host: match[1],
+				port: portNumber,
+				secure: isHTTPS
+			}, req).request(function(err, result) {
+				var session = result || {};
+				sessionStore[req.sessionID].data = session;
+				req.session = session;
+				next();
+			}, 'POST', peer2peer, {}, {sessionKey: sessionKey, action: ACTION.SESSION});
+		} else {
+			req.session = sessionStore[req.sessionID].data;
+			next();
+		}
+	});
+
+	//clean session storage on local machine 
+	if (expInterval !== -1) {
+		setInterval(apis.cleanAll, expInterval * 1000);
+	}
+
+	return apis;
+};
---
+++ tests/index.js
@@ -0,0 +1,241 @@
+'use strict';
+
+var session = require('../index'),
+	assert = require('assert'),
+	fs = require('fs');
+
+describe('Testing Session', function () {
+
+	var opt = {
+		'isHTTPS': false,
+		'session-env': true, 
+		'session-key': 'mySecretKey',
+		'session-name': 'my.sid',
+		'peer2peer': '/cloud/session',
+		'session-file': './session',
+		'exp-time': 3600,
+		'exp-interval': -1
+	}, app = {
+		use: function() {
+		},
+		post: function() {
+		}
+	}, req = {
+		headers: {},
+		query: { action: 'TEST', sessionKey: opt['session-key'] }
+	}, res = {
+		send: function() {
+		},
+		cookie: function() {
+		}
+	}, ipaddress;
+
+	it('Should test session with default options', function(done) {
+		session(app, 12345, null);
+		done();
+	});
+
+	it('Should test session with user options', function(done) {
+		session(app, 12345, opt);
+		done();
+	});
+
+	it('Should test serialize JSON data', function(done) {
+		var apis = session(app, 12345, opt),
+			data = apis.serialize();
+		assert.equal(data, '""');
+		data = apis.serialize(null, null, {'key':'value'});
+		assert.equal(data, '{"key":"value"}');
+		done();
+	});
+
+	it('Should test deserialize JSON data', function(done) {
+		var apis = session(app, 12345, opt),
+			data = apis.deserialize();
+		assert.ok(typeof data === 'object');
+		data = apis.deserialize(null, null, '{"key":"value"}');
+		assert.equal(data.key, 'value');
+		done();
+	});
+
+	it('Should test encrypt token', function(done) {
+		var apis = session(app, 12345, opt),
+			data = apis.encrypt ('df3c50b1c2eda61617457e5646e36f25', opt['session-key']);
+		assert.equal(data, 'SzWSzd5DZiA9yrkMBhqgyPZU4CRaqC03xQp6mu1hXrg=');
+		done();
+	});
+
+	it('Should test destroy current session', function(done) {
+		var proxy = function() {
+			return {
+				request: function(callback) {
+					callback(null);
+				}
+			};
+		},
+		apis = session(app, 12345, opt, proxy);
+
+		apis.destroy({ headers: {cookie: ''} });
+		apis.destroy({ headers: {cookie: 'x-cloud-ipaddress=192.168.1.1'} }, function(err) {
+			assert.equal(err, null);
+			done();
+		});
+	});
+
+	it('Should test post request without cookie', function(done) {
+		app.post = function(path, callback) {
+			assert.equal(path, opt.peer2peer);
+			callback(req, res);
+		}
+
+		session(app, 12345, opt);
+		done();
+	});
+
+	it('Should test post request with invalid cookie', function(done) {
+		req.headers.cookie = ' ';
+		app.post = function(path, callback) {
+			assert.equal(path, opt.peer2peer);
+			callback(req, res);
+		}
+
+		session(app, 12345, opt);
+		done();
+	});
+
+	it('Should test post request with session id', function(done) {
+		req.headers.cookie = opt['session-name'] + '=1234567890';
+		app.post = function(path, callback) {
+			assert.equal(path, opt.peer2peer);
+			callback(req, res);
+		}
+		session(app, 12345, opt);
+		done();
+	});
+
+	it('Should test post transfer_sesssion action', function(done) {
+		req.query.action = 'transfer_sesssion';
+		app.post = function(path, callback) {
+			assert.equal(path, opt.peer2peer);
+			callback(req, res);
+		}
+		session(app, 12345, opt);
+		done();
+	});
+
+	it('Should test app middleware with existing session', function(done) {
+		var _req = JSON.parse(JSON.stringify(req));
+		app.use = function(callback) {
+			_req.session = {};
+			callback(_req, res, done);
+		}
+		session(app, 12345, opt);
+	});
+
+	it('Should test app middleware with assigned sessionID', function(done) {
+		var _req = JSON.parse(JSON.stringify(req));
+		app.use = function(callback) {
+			_req.sessionID = '54321';
+			callback(_req, res, function() {
+				done();
+			});
+		}
+		session(app, 12345, opt);
+	});
+
+	it('Should test app middleware without cookie', function(done) {
+		var _req = JSON.parse(JSON.stringify(req));
+		app.use = function(callback) {
+			_req.headers.cookie = null;
+			callback(_req, res, done);
+		}
+		session(app, 12345, opt);
+	});
+
+	it('Should test app middleware with invalid cookie', function(done) {
+		var _req = JSON.parse(JSON.stringify(req));
+		app.use = function(callback) {
+			_req.headers.cookie = ' ';
+			callback(_req, res, done);
+		}
+		session(app, 12345, opt);
+	});
+
+	it('Should test app middleware x-cloud-ipaddress', function(done) {
+		var _req = JSON.parse(JSON.stringify(req)),
+			proxy = function() {
+				return {
+					request: function(callback) {
+						callback(null, {time: Date.now(), data: {}});
+					}
+				};
+			};
+		_req.headers.cookie += ';x-cloud-ipaddress=192.255.255.255';
+		app.use = function(callback) {
+			callback(_req, res, done);
+		}
+		session(app, 12345, opt, proxy);
+	});
+
+	it('Should test app middleware x-cloud-ipaddress invalid session', function(done) {
+		var _req = JSON.parse(JSON.stringify(req)),
+			proxy = function() {
+				return {
+					request: function(callback) {
+						callback({status:'ERROR'});
+					}
+				};
+			};
+		_req.headers.cookie += ';x-cloud-ipaddress=192.255.255.255';
+		app.use = function(callback) {
+			callback(_req, res, done);
+		}
+		session(app, 12345, opt, proxy);
+	});
+
+	it('Should test app middleware save session to file', function(done) {
+		process.env.NODE_ENV = 'development';
+		var _req = JSON.parse(JSON.stringify(req));
+		fs.unlink(opt['session-file'], function() {
+			app.use = function(callback) {
+				callback(_req, res, done);
+			}
+			session(app, 12345, opt);
+		});
+	});
+
+	it('Should test app middleware read session from file', function(done) {
+		var _req = JSON.parse(JSON.stringify(req));
+		app.use = function(callback) {
+			callback(_req, res, done);
+		}
+		session(app, 12345, opt);
+	});
+
+	it('Should test app middleware cleanAll function', function(done) {
+		var _req = JSON.parse(JSON.stringify(req));
+		app.use = function(callback) {
+			callback(_req, res, function() {});
+		}
+
+		var apis = session(app, 12345, opt);
+		apis.cleanAll();
+		done();
+	});
+
+	it('Should test app middleware expire session', function(done) {
+		var _req = JSON.parse(JSON.stringify(req)),
+			apis = null;
+		app.use = function(callback) {
+			setTimeout(function() {
+				apis.serialize = function(req, res, session) {
+					session.time -= opt['exp-time'] * 1000;
+					return JSON.stringify(session);
+				},
+				callback(_req, res, done);
+				apis.cleanAll();
+			}, 1);				
+		}
+		apis = session(app, 12345, opt);
+	});
+});
